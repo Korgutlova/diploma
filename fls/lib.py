f23c54e2d939bafa4f935c100db787e660b7e77a
Ivan <iashevyakov@mail.ru>
2019-03-01 00:49:02 +0300

    calc criterion value by user's formula

--- fls/lib.py
+++ fls/lib.py
@@ -1,27 +1,44 @@
 import math
 import cexprtk
+import django
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dipl.settings")
+django.setup()
 
 from py_expression_eval import Parser
 
+from fls.models import CriterionValue, ParamValue
+
 parser = Parser()
 
-#типовая
+
+# типовая
 def func1(a, b):
-    return float(math.log(a, 2)) + b
+    return float(math.log(a, 2)) / (b + 53)
 
 
-def parse_formula(formula):
+def parse_formula(formula, params_values):
     vars_func = parser.parse(formula).variables()
     vars = list(filter(lambda x: x.find('_') != -1, vars_func))
     funcs = list(filter(lambda x: x.find('_') == -1, vars_func))
     variables = {}
     for var in vars:
-        variables[var] = 5
+        variables[var] = params_values[int(var.split('_')[1])]
     st = cexprtk.Symbol_Table(variables)
     for func in funcs:
         st.functions[func] = globals()[func]
     calc_exp = cexprtk.Expression(formula, st)
     print(calc_exp())
+    return calc_exp()
 
 
-parse_formula("5*a_1 + func1(a_2, a_3)/log(a_4)")
+def calc_criterion_value(criterion, group):
+    params = criterion.competition.competition_params
+    param_values = []
+    for param in params:
+        param_values.append(ParamValue.objects.get(param=param, group=group).value)
+    value = parse_formula(criterion.formula, param_values)
+    CriterionValue.objects.create(criterion=criterion, group=group, value=value)
+
+
+parse_formula("5*a_0 + func1(a_1, a_2)/log(a_3)", [2, 8, 7, 5])
--- fls/views.py
+++ fls/views.py
@@ -5,7 +5,8 @@ from fls.models import Param
 
 def params(request):
     if request.method == 'POST':
-        for text, desc, min, max in zip(request.POST['text'], request.POST['description'], request.POST['min'],
-                                        request.POST['max']):
+        print(request.POST.getlist('text'))
+        for text, desc, min, max in zip(request.POST.getlist('text'), request.POST.getlist('description'), request.POST.getlist('min'),
+                                        request.POST.getlist('max')):
             Param.objects.create(name=text, description=desc, min=min, max=max)
     return render(request, 'fls/add_params.html', {})
