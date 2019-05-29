3fff2d842e2f8f05a5cf16c68ad99d2747d17f4d
Ivan <iashevyakov@mail.ru>
2019-05-29 22:16:05 +0300

    cmp app fixes

--- cmp/lib.py
+++ cmp/lib.py
@@ -1,4 +1,3 @@
-import copy
 import pickle
 import random
 from random import randint
@@ -6,74 +5,27 @@ import os
 import django
 import numpy as np
 
-from cmp.lib2 import calculate_est
+from cmp.lib2 import calculate_estimations
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dipl.settings")
 django.setup()
 
-from cmp.models import Weights, GroupWeights
+input_file_template = './static/cmp_results/%s'
 
-COUNT = 20
+output_file_template = './static/cmp_results/weights-%s'
 
-input_file_template = './static/result/%s'
-
-
-def smart_gen():
-    const_act = [0, 5, 10, 39, 40]
-    d1_part = [(1, 4), (6, 9), (11, 12), (13, 14), (35, 38), (41, 44)]
-    olimp = [(15, 18, 5), (19, 22, 6), (23, 26, 7), (27, 30, 8), (31, 34, 4)]
-    coef = {}
-    for ind in const_act:
-        coef[ind] = random.randint(1, 9)
-    for ind_st, ind_end in d1_part:
-        ind = ind_end
-        est = random.randint(4, 9)
-        while ind >= ind_st:
-            coef[ind] = est
-            ind -= 1
-            est -= 1
-    for ind_st, ind_end, est_st in olimp:
-        ind = ind_end
-        est = est_st
-        while ind >= ind_st:
-            coef[ind] = est
-            ind -= 1
-            est -= 1
-    coef = dict(sorted(coef.items(), key=lambda item: item[0]))
-    return list(coef.values())
-
-
-def cmp(weights, param='kfavg', group=-1):
+def cmp(weights, param='kfavg'):
     input_file = open(input_file_template % param, 'rb')
     data, name_groups, old_ranking, academic_performance = pickle.load(input_file)
 
-    if group != -1:
-        data.pop(group)
-        name_groups.pop(group)
-        old_ranking.pop(group)
-        academic_performance.pop(group)
-    new_ranking = calculate_est(data, weights)
+    new_ranking = calculate_estimations(data, weights)
 
-    return old_ranking, new_ranking, [abs(old - new) for old, new in zip(old_ranking, new_ranking)], range(1,16)
-
-
-def put_best(weights, sum, param='kfavg'):
-    objects = Weights.objects.filter(type=param).order_by('-deviations_sum')
-    if objects.count() < COUNT:
-        Weights.objects.create(weights=' '.join(str(e) for e in weights), deviations_sum=sum, type=param)
-    elif objects.first().deviations_sum > sum:
-        objects.first().delete()
-        Weights.objects.create(weights=' '.join(str(e) for e in weights), deviations_sum=sum, type=param)
-
-
-def generate_random_weights(iter_count):
-    random_weights = [[randint(1, 9) for i in range(45)] for i in range(iter_count)]
-    for weights in random_weights:
-        put_best(weights, sum(cmp(weights, param='c')[2]), param='c')
+    return old_ranking, new_ranking, [abs(old - new) for old, new in zip(old_ranking, new_ranking)], range(1, len(
+        new_ranking) + 1)
 
 
 def fitness(set_weights, data, old_ranking):
-    return [sum([abs(old - new) for old, new in zip(old_ranking, calculate_est(data, weights))]) for weights in
+    return [sum([abs(old - new) for old, new in zip(old_ranking, calculate_estimations(data, weights))]) for weights in
             set_weights]
 
 
@@ -82,13 +34,6 @@ def mating_pool(pop, fitness, num_parents):
     ind = np.argpartition(fitness, num_parents)[-num_parents:]
     ind = ind[np.argsort(fitness[ind])]
     return pop[ind]
-
-
-def best_weights(pop, fitness, num_best):
-    fitness, pop = np.array(fitness), np.array(pop)
-    ind = np.argpartition(fitness, num_best)[-num_best:]
-    ind = ind[np.argsort(fitness[ind])]
-    return list(pop[ind]), list(fitness[ind])
 
 
 def crossover(parents, offspring_size):
@@ -115,12 +60,12 @@ def mutation(offsprings, data):
     for off_index in range(offsprings.shape[0]):
         offspring_values = list(offsprings[off_index, :])
         offspring_values[gene_index] = random.randint(1, 9)
-        if calculate_est(data, list(offsprings[off_index, :])) > calculate_est(data, offspring_values):
+        if calculate_estimations(data, list(offsprings[off_index, :])) > calculate_estimations(data, offspring_values):
             offsprings[off_index, :] = offspring_values
     return offsprings
 
 
-def genetic_algoritm(param='avg'):
+def genetic_algorithm(param='avg'):
     input_file = open(input_file_template % param, 'rb')
     data, name_groups, old_ranking, academic_performance = pickle.load(input_file)
     num_weights = 45
@@ -132,8 +77,6 @@ def genetic_algoritm(param='avg'):
 
     population = np.array([[randint(1, 9) for i in range(num_weights)] for i in range(sol_per_pop)])
 
-    # population = np.array([smart_gen() for i in range(sol_per_pop)])
-
     fitnesses = []
     for generation in range(num_generations):
         fitnesses = fitness(population, data, old_ranking)
@@ -142,17 +85,12 @@ def genetic_algoritm(param='avg'):
         offspring_mutation = mutation(offsprings, data)
         population[0:parents.shape[0], :] = parents
         population[parents.shape[0]:, :] = offspring_mutation
-    weights, fits = best_weights(population, fitnesses, 20)
-    print(weights)
-    print(fits)
-    for b_w, fit in zip(weights, fits):
-        Weights.objects.create(weights=' '.join(str(e) for e in b_w), deviations_sum=fit, type=param, ga=True)
-    return population
+    min_fitness_value_index = fitnesses.index(min(fitnesses))
+    print(fitnesses)
+    file = open(output_file_template % param, 'wb')
+    pickle.dump(' '.join(str(weight) for weight in population[min_fitness_value_index]), file)
+
+    return population, fitnesses
 
 
-# print(mating_pool([3, 7, 2, 4, 1], [80, 50, 5, 10, 40], 2))
-
-# print(smart_gen())
-
-# for param in ['avg', 'kfavg', 'c', 'kf']:
-#     genetic_algoritm(param)
+# print(genetic_algorithm())
--- cmp/models.py
+++ cmp/models.py
@@ -2,23 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-TYPES = (
-    ('kfavg', 'KF+AVG'),
-    ('kf', 'KF'),
-    ('avg', 'AVG'),
-    ('c', 'COUNT')
-)
-
-
-class Weights(models.Model):
-    weights = models.TextField()
-    deviations_sum = models.FloatField()
-    type = models.CharField(choices=TYPES, default=TYPES[0][0], max_length=10)
-    ga = models.BooleanField(default=False)
-
-
-class GroupWeights(models.Model):
-    group_name = models.CharField(max_length=30)
-    weights = models.TextField()
-    deviations_sum = models.FloatField()
-    type = models.CharField(choices=TYPES, max_length=10)
